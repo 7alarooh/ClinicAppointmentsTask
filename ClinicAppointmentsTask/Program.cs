4d96bea4e94b432c044e57f690d7b9a0f380f8ea
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-11 13:17:10 +0400

    feat: configure Program.cs for dependency injection and service registration

--- ClinicAppointmentsTask/Program.cs
+++ ClinicAppointmentsTask/Program.cs
@@ -1,4 +1,6 @@
 
+using ClinicAppointmentsTask.Repositories;
+using ClinicAppointmentsTask.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClinicAppointmentsTask
@@ -11,7 +13,14 @@ namespace ClinicAppointmentsTask
 
             // Add services to the container.
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IBookingService, BookingService>();
+            builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+            builder.Services.AddScoped<IPatientService, PatientService>();
+            builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+            builder.Services.AddScoped<IClinicService, ClinicService>();
+            builder.Services.AddScoped<IClinicRepository, ClinicRepository>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,7 +39,6 @@ namespace ClinicAppointmentsTask
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
