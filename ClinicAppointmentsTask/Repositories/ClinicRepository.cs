541af2bc455499767da56776e25a220e5b49549f
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-11 12:26:22 +0400

    feat: add ClinicRepository with CRUD operations and IClinicRepository interface

---
+++ ClinicAppointmentsTask/Repositories/ClinicRepository.cs
@@ -0,0 +1,51 @@
+﻿using ClinicAppointmentsTask.Models;
+
+namespace ClinicAppointmentsTask.Repositories
+{
+    public class ClinicRepository : IClinicRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClinicRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get all clinics
+        public IEnumerable<Clinic> GetAllClinics()
+        {
+            return _context.Clinics.ToList();
+        }
+
+        // Get a clinic by ID
+        public Clinic? GetClinicById(int id)
+        {
+            return _context.Clinics.FirstOrDefault(c => c.ClinicId == id);
+        }
+
+        // Add a new clinic
+        public void AddClinic(Clinic clinic)
+        {
+            _context.Clinics.Add(clinic);
+            _context.SaveChanges();
+        }
+
+        // Update an existing clinic
+        public void UpdateClinic(Clinic clinic)
+        {
+            _context.Clinics.Update(clinic);
+            _context.SaveChanges();
+        }
+
+        // Delete a clinic
+        public void DeleteClinic(int id)
+        {
+            var clinic = GetClinicById(id);
+            if (clinic != null)
+            {
+                _context.Clinics.Remove(clinic);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClinicAppointmentsTask/Repositories/IClinicRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClinicAppointmentsTask.Models;
+
+namespace ClinicAppointmentsTask.Repositories
+{
+    public interface IClinicRepository
+    {
+        IEnumerable<Clinic> GetAllClinics();
+        Clinic? GetClinicById(int id);
+        void AddClinic(Clinic clinic);
+        void UpdateClinic(Clinic clinic);
+        void DeleteClinic(int id);
+    }
+}
