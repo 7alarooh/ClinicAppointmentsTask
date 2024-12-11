aa8338a4fc1280e3ea99213530881aac07cdc037
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-11 10:11:15 +0400

    create Clinic table with the basic cloumns

---
+++ ClinicAppointmentsTask/Models/Clinic.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ClinicAppointmentsTask.Models
+{
+    public class Clinic
+    {
+        [Key]
+        public int ClinicId { get; set; }
+        [Required]
+        public string ClinicSepcialization { get; set; }
+        [Required]
+
+        [Range(0, 20, ErrorMessage = "Limit reached for today!")]
+        public int numberOfSlots { get; set; } = 0;
+ }
+}
