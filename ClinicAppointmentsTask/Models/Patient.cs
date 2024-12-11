f6f1b8e5b6f0113a7795275036f30e23b2882820
Azzak <azzaalmahrooqi79@gmail.com>
2024-12-11 09:55:03 +0400

    create Patient table with the basic cloumns

---
+++ ClinicAppointmentsTask/Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicAppointmentsTask.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class Patient
+    {
+        [Key]
+        public int PatientId { get; set; }
+        [Required]
+        public string PatientName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        [EnumDataType(typeof(Gender), ErrorMessage = "Invalid gender.")]
+        public Gender Gender { get; set; } // Gender of the user (e.g., Male, Female)
+
+
+    }
+}
